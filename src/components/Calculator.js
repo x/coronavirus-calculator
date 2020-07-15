8623fa17bb8b65ba47c77b532a1a09b4899ab0b2
Devon Peticolas <devon@oden.io>
2020-07-14 23:18:49 -0400

    🔥

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,49 +6,102 @@ import "../styles/Calculator.css";
 import "react-input-range/lib/css/index.css";
 
 class Calculator extends Component {
+
+  DAYS_CONTAGIOUS_DEFAULT = 10;
+  /*
+   * Use this multiplier to convert dailyCases per day to the number of circulating cases. This
+   * default the following...
+   * 1. A patient is infectious for 3 days before the onset of symptoms [a] and 7 days after [2].
+   *    a. https://www.acpjournals.org/doi/10.7326/M20-0504
+   *    b. https://www.thelancet.com/journals/laninf/article/PIIS1473-3099(20)30196-1/fulltext
+   * 2. Those who test positive for COVID19 do not change their behavior after testing positive.
+   *    This is a bad assumption but I haven't yet found data on number of days after onset of
+   *    symptoms someone gets tested or the portion of people who change their behavior.
+   */
+
+   PERCENT_REPORTED = 10
+   /*
+    * This based on CDC Director Robert Redfield's comment at the end of June [a] that a good rough
+    * estimate of actual cases to reported cases is 10-1.
+    * a. https://www.cdc.gov/media/releases/2020/t0625-COVID-19-update.html
+    */
+
   state = {
-    dailyCasesValue: 5000,
-    populationValue: 1000000,
-    groupValue: 25
+    populationValue: 19450000, // NY
+    dailyCasesValue: 560, // NY 7-day average (July 14th)
+    groupValue: 25,
+    daysContagiousValue: this.DAYS_CONTAGIOUS_DEFAULT,
+    percentReportedValue: this.PERCENT_REPORTED,
   };
 
-  handledailyCasesChange = value => {
-    this.setState({ dailyCasesValue: value });
-  };
-  handlePopulationChange = value => {
+  handlePopulationChange = (value) => {
     this.setState({ populationValue: value });
   };
-  handlegroupChange = value => {
+  handleDailyCasesChange = (value) => {
+    this.setState({ dailyCasesValue: value });
+  };
+  handleDaysContagiousChange = (value) => {
+    this.setState({ daysContagiousValue: value });
+  };
+  handlePartReportedChange = (value) => {
+    this.setState({ percentReportedValue: value });
+  };
+  handlegroupChange = (value) => {
     this.setState({ groupValue: value });
   };
 
-
   render() {
-    const { dailyCasesValue, populationValue, groupValue } = this.state;
+    const {
+      populationValue,
+      dailyCasesValue,
+      daysContagiousValue,
+      percentReportedValue,
+      groupValue,
+    } = this.state;
 
     return (
       <div className="App">
-        <h4>My state has a 7-day rolling average of {dailyCasesValue} dailyCases per day</h4>
-        <InputRange
-          step={100}
-          maxValue={20000}
-          minValue={0}
-          value={dailyCasesValue}
-          onChange={this.handledailyCasesChange}
-        />
-        <h4>
-          My state as a population of {populationValue}
-        </h4>
+        <h4>My state as a population of {populationValue.toLocaleString()}</h4>
         <InputRange
           step={50000}
-          maxValue={10000000}
+          maxValue={40000000}
           minValue={50000}
           value={populationValue}
           onChange={this.handlePopulationChange}
         />
         <h4>
-          I'm attending an event with {groupValue} people
+          My state has a 7-day rolling average of {dailyCasesValue.toLocaleString()} daily cases
+          per day
         </h4>
+        <InputRange
+          step={100}
+          maxValue={20000}
+          minValue={0}
+          value={dailyCasesValue}
+          onChange={this.handleDailyCasesChange}
+        />
+        <h4>
+          A person who tested positive for the virus is contagious for{" "}
+          {daysContagiousValue} days
+        </h4>
+        <InputRange
+          step={1}
+          maxValue={30}
+          minValue={1}
+          value={daysContagiousValue}
+          onChange={this.handleDaysContagiousChange}
+        />
+        <h4>
+          Only {percentReportedValue}% of cases are reported
+        </h4>
+        <InputRange
+          step={1}
+          maxValue={100}
+          minValue={1}
+          value={percentReportedValue}
+          onChange={this.handlePartReportedChange}
+        />
+        <h4>I'm attending an event with {groupValue.toLocaleString()} people</h4>
         <InputRange
           step={1}
           maxValue={300}
@@ -56,7 +109,13 @@ class Calculator extends Component {
           value={groupValue}
           onChange={this.handlegroupChange}
         />
-        <Display population={populationValue} dailyCases={dailyCasesValue} group={groupValue} />
+        <Display
+          population={populationValue}
+          dailyCases={dailyCasesValue}
+          daysContagious={daysContagiousValue}
+          percentReported={percentReportedValue}
+          group={groupValue}
+        />
       </div>
     );
   }
--- src/components/Display.js
+++ src/components/Display.js
@@ -3,25 +3,27 @@ import PropTypes from "prop-types";
 import DisplayChild from "./DisplayChild";
 
 class Display extends Component {
-  NEW_CASES_PER_DAY_TO_CIRCULATING = 10;
-  /*
-   * Use this multiplier to convert dailyCases per day to the number of active dailyCases. This number assumes the following...
-   * 1. A patient is infectious for 3 days before the onset of symptoms [a] and 7 days after [2].
-   *    a. https://www.acpjournals.org/doi/10.7326/M20-0504
-   *    b. https://www.thelancet.com/journals/laninf/article/PIIS1473-3099(20)30196-1/fulltext
-   * 2. Those who test positive for COVID19 do not change their behavior after testing positive.
-   *    This is a bad assumption but I haven't yet found data on number of days after onset of
-   *    symptoms someone gets tested or the portion of people who change their behavior.
-   */
-
-  CASES_REPORTED_PER_ACTUAL_CASE = 0.05
+  CASES_REPORTED_PER_ACTUAL_CASE = 0.05;
   // 1 in 20
 
   odds = () => {
-    // https://marginalrevolution.com/marginalrevolution/2020/03/covid-19-event-risk-assessment-planner.htmljkjkjk
-    const { dailyCases, population, group } = this.props;
-    const cases = dailyCases * this.NEW_CASES_PER_DAY_TO_CIRCULATING / this.CASES_REPORTED_PER_ACTUAL_CASE;
-    const prob = 1 - Math.pow(1 - cases / population, group);
+    // https://marginalrevolution.com/marginalrevolution/2020/03/covid-19-event-risk-assessment-planner.html
+    const {
+      population,
+      dailyCases,
+      daysContagious,
+      percentReported,
+      group,
+    } = this.props;
+    const circulatingCases = (dailyCases * daysContagious) / (percentReported / 100);
+    const prob = 1 - Math.pow(1 - (circulatingCases / population), group);
+
+    if (prob >= 0.9999) {
+      // Never be 100% sure of anything...
+      return <p>{">"}99.99%</p>;
+    }
+
+    // Nice formatting
     return <p>{(prob * 100).toFixed(2)}%</p>;
   };
 
@@ -29,10 +31,6 @@ class Display extends Component {
     return (
       <div className="flex">
         <DisplayChild func={this.odds()} text="chance someone has COVID" />
-        {/* <DisplayChild
-          func={this.calculateMonthlyRepayment()}
-          text=" monthly repayment"
-        /> */}
       </div>
     );
   }
@@ -41,7 +39,9 @@ class Display extends Component {
 Display.propTypes = {
   dailyCasesValue: PropTypes.number.isRequired,
   populationValue: PropTypes.number.isRequired,
-  groupValue: PropTypes.number.isRequired
+  daysContagious: PropTypes.number.isRequired,
+  percentReported: PropTypes.number.isRequired,
+  groupValue: PropTypes.number.isRequired,
 };
 
 export default Display;
